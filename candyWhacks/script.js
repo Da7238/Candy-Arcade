c39a83884b5acb780718eeab9513e8b5eef08465
Annie Tiet <anniedtiet@gmail.com>
2022-10-15 13:02:52 -0400

    timer

--- candyWhacks/script.js
+++ candyWhacks/script.js
@@ -32,3 +32,29 @@ run();
 function getRandomNumRange(start, end) {
     return Math.random() * (end - start) + start;
 }
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+
+      if (--timer < 0) {
+        alert("game over");
+        timer = duration;
+        points = 0;
+        pointsEl.textContent = points;
+      }
+    }, 1000);
+  }
+
+  window.onload = function () {
+    let time = 30,
+    display = document.querySelector('#time');
+    startTimer(time, display);
+  };
